47c0751c41c99d3a35027ee73c8976d8caf7ab53
Camila Alani <alanicamila124@gmail.com>
2024-01-31 01:39:58 -0300

    agregue los imports y los states para los datos del formulario que son el nombre, descripcion y genero

--- src/components/FormularioPelicula.jsx
+++ src/components/FormularioPelicula.jsx
@@ -1,7 +1,10 @@
+import React, { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
 
-
-const FormularioPelicula = () => {
-
+const FormularioPelicula = ({ onAgregarPelicula }) => {
+    const [nombre, setNombre] = useState("");
+    const [descripcion, setDescripcion] = useState("");
+    const [genero, setGenero] = useState("comedia");
 
   return (
     <>
