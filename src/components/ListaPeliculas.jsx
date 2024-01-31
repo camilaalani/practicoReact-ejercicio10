38998d5eab6981147de74dedb1296e92e8788e27
Camila Alani <alanicamila124@gmail.com>
2024-01-31 01:42:00 -0300

    agregue los imports, y en mediante props recibe peliculas y onBorrarPelicula. Luego dentro del card body se ve el encabezado de lista de peliculas y verifica si hay peliculas registradas, en caso de que no las haya muestra un mensaje, de lo contrario mapea las peliculas y renderiza el componente PeliculaCard para cada una

--- src/components/ListaPeliculas.jsx
+++ src/components/ListaPeliculas.jsx
@@ -1,10 +1,27 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import PeliculaCard from "./PeliculaCard";
 
-
-const ListaPeliculas = () => {
+const ListaPeliculas = ({ peliculas, onBorrarPelicula }) => {
   return (
-    <>
-      
-    </>
+    <Card>
+      <Card.Body>
+        <h2>Lista de Películas</h2>
+        {peliculas.length === 0 ? (
+          <p className="text-center mt-3">No hay películas registradas</p>
+        ) : (
+          <>
+            {peliculas.map((pelicula, index) => (
+              <PeliculaCard
+                key={index}
+                pelicula={pelicula}
+                onBorrar={() => onBorrarPelicula(index)}
+              />
+            ))}
+          </>
+        )}
+      </Card.Body>
+    </Card>
   );
 };
 
