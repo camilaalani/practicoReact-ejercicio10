fd2e9c3c94a7248bbadbeeb88a734781d29689cd
Camila Alani <alanicamila124@gmail.com>
2024-01-31 01:42:38 -0300

    agregue los imports, luego pase por props pelicula y onBorrar, tambien agregue una card donde en el card body muestra el titulo de la pelicula, la descripcion y el genero y por ultimo el boton que ejecuta la funcion onBorrar

--- src/components/PeliculaCard.jsx
+++ src/components/PeliculaCard.jsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
-const PeliculaCard = () => {
+const PeliculaCard = ({ pelicula, onBorrar }) => {
   return (
-    <>
-      
-    </>
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Película: {pelicula.nombre}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          Género: {pelicula.genero}
+        </Card.Subtitle>
+        <Card.Text>
+          <strong>Descripción:</strong> {pelicula.descripcion}
+        </Card.Text>
+        <Button variant="danger" onClick={onBorrar}>
+          Borrar
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
